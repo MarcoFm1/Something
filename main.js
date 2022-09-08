4cc81626a293c689e4a81b1d33bf5bfd3fa428ba
Marco Fini Minue <marco.fini22@gmail.com>
2022-09-08 20:26:20 -0300

    nothing

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function myFunction() {
-    var element = document.body;
+    var element = document.getElementsByClassName('sw').style.backgroundColor = 'black';
     element.classList.toggle("dark-mode");
  }
